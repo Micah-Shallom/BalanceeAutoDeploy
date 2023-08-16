b27bf12b857933f5da90d0b6a163d473d488a467
Micah <micahshallom@gmail.com>
2023-08-16 19:41:12 +0100

    created security groups for production infrastructure

---
+++ infrastructure/prod/modules/security/outputs.tf
@@ -0,0 +1,24 @@
+output "ALB-sg" {
+  value = aws_security_group.RCR["ext-alb-sg"].id
+}
+
+
+output "IALB-sg" {
+  value = aws_security_group.RCR["int-alb-sg"].id
+}
+
+
+output "bastion-sg" {
+  value = aws_security_group.RCR["bastion-sg"].id
+}
+
+
+output "nginx-sg" {
+  value = aws_security_group.RCR["nginx-sg"].id
+}
+
+
+output "web-sg" {
+  value = aws_security_group.RCR["webserver-sg"].id
+}
+
