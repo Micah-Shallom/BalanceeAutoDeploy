5b76962253736a585a315873dd9897a863af7417
Micah <micahshallom@gmail.com>
2023-08-16 20:34:53 +0100

    created the ALB module which routes all traffic appropriately

--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -1,5 +1,25 @@
+module "ALB" {
+  source             = "./modules/alb"
+  name1               = "RCR-ext-alb"
+  name2               = "RCR-int-alb"
+  vpc_id             = module.VPC.vpc_id
+  public-sg          = module.security.ALB-sg
+  private-sg         = module.security.IALB-sg
+  public-sbn-1       = module.VPC.public_subnets-1
+  public-sbn-2       = module.VPC.public_subnets-2
+  private-sbn-1      = module.VPC.private_subnets-1
+  private-sbn-2      = module.VPC.private_subnets-2
+  load_balancer_type = "application"
+  ip_address_type    = "ipv4"
+}
+
+module "security" {
+  source = "./modules/security"
+  vpc_id = module.VPC.vpc_id
+}
+
 module "VPC" {
-  source                              = "./modules/VPC"
+  source                              = "./modules/vpc"
   region                              = var.region
   vpc_cidr                            = var.vpc_cidr
   enable_dns_support                  = var.enable_dns_support
@@ -7,7 +27,7 @@ module "VPC" {
   preferred_number_of_public_subnets  = var.preferred_number_of_public_subnets
   preferred_number_of_private_subnets = var.preferred_number_of_private_subnets
   public_subnets                      = [for i in range(2, 5, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
-  # private_subnets                     = [for i in range(1, 8, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
   private_subnets = [for i in range(1, 8, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
-  # private_subnets                     = cidrsubnet(var.vpc_cidr, 8, count.index+2)
 }
+
+
---
+++ infrastructure/prod/modules/alb/alb.tf
@@ -0,0 +1,117 @@
+###################################################################################
+# ----------------------------
+#External Load Balancers for webservers
+#---------------------------------
+####################################################################################
+resource "aws_lb" "ext-alb" {
+  name     = var.name1
+  internal = false
+  security_groups = [
+    var.public-sg
+  ]
+
+  subnets = [
+    var.public-sbn-1,
+    var.public-sbn-2
+  ]
+
+  tags = merge(
+    var.tags,
+    {
+      Name = var.name1
+    },
+  )
+
+  ip_address_type    = var.ip_address_type
+  load_balancer_type = var.load_balancer_type
+}
+
+#-------- Creating the nginx target group -------------
+resource "aws_lb_target_group" "nginx-tgt" {
+  health_check {
+    interval            = 10
+    path                = "/healthstatus"
+    protocol            = "HTTPS"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+  name        = "nginx-tgt"
+  port        = 443
+  protocol    = "HTTPS"
+  target_type = "instance"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_lb_listener" "nginx-listner" {
+  load_balancer_arn = aws_lb.ext-alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate_validation.shallom.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx-tgt.arn
+  }
+}
+
+###################################################################################
+# ----------------------------
+#Internal Load Balancers for webservers
+#---------------------------------
+####################################################################################
+resource "aws_lb" "int-alb" {
+  name     = var.name2
+  internal = true
+  security_groups = [
+    var.private-sg
+  ]
+
+  subnets = [
+    var.private-sbn-1,
+    var.private-sbn-2
+  ]
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "RCR-int-alb"
+    },
+  )
+
+  ip_address_type    = var.ip_address_type
+  load_balancer_type = var.load_balancer_type
+}
+
+# --- target group  for application -------
+
+resource "aws_lb_target_group" "application-tgt" {
+  health_check {
+    interval            = 10
+    path                = "/healthstatus"
+    protocol            = "HTTPS"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  name        = "application-tgt"
+  port        = 443
+  protocol    = "HTTPS"
+  target_type = "instance"
+  vpc_id      = var.vpc_id
+}
+
+# For this aspect a single listener was created for the wordpress which is default
+
+resource "aws_lb_listener" "web-listener" {
+  load_balancer_arn = aws_lb.int-alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate_validation.shallom.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.application-tgt.arn
+  }
+}
